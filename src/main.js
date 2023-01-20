8979f597f1f778d482c6e05e9f1fc28bec45f347
Mansur <vakhabov.mans@gmail.com>
2023-01-20 23:29:03 +0300

    настройка для глоб.регистрации компонентов

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-// import router from "./router";
 import store from "./store";
 import "./assets/CSS/main.css";
 
-createApp(App).use(store).mount("#app");
+const app = createApp(App).use(store).mount("#app");
